ba75618ee83b21dc1ff95d1217714800b07b6f4a
Kevin Bell <bellKevin@pm.me>
2022-10-07 09:07:51 -0600

    Update ch6celsiusTemperatures.cpp

--- ch6celsiusTemperatures/ch6celsiusTemperatures/ch6celsiusTemperatures.cpp
+++ ch6celsiusTemperatures/ch6celsiusTemperatures/ch6celsiusTemperatures.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void tempConvert(int aValue);
 int main()
 {
-    int number=0;
+    auto number = 0;
     cout << "Chap. 6 Celsius Temperatures by Kevin Bell\n";
     cout << "Temperature Conversion Table\n\n";
     cout << "    Fahrenheit        Celsius   \n";
@@ -16,12 +16,10 @@ int main()
     return 0;
 }
 void tempConvert(int number) {
-    double celsius=0.0;
-    int fahrenheit=0;
-    celsius = (.55555555556) * (fahrenheit - 32);
-    int num;
+    auto celsius = 0.0;
+    int fahrenheit = 0, num;
     for (num = 25; num < 100; num) {
         num = num + 5;
-        cout << setw(10) <<setprecision(2)<< fixed << num << setw(18) << (num * .6214) << endl;
+        cout << setw(10) <<setprecision(2)<< fixed << num << setw(18) << ((.55555555556) * (num - 32)) << endl;
     }
 }
